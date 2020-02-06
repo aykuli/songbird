ae8cb2a6297b8c5a2c681b18f9e63e2f2b119114
Aykuli <aykuli@ya.ru>
2020-02-06 23:54:45 +0700

    feat choose audio player

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/src/styles.scss';
+
+import ReactAudioPlayer from 'react-audio-player';
 
 import './player.scss';
 
 const Player = (props) => {
     const { src, birdName, audioSource } = props;
     return (
-        <div className="player">
+        <div className="guess">
             <img
-                className="player__img"
+                className="guess__img"
                 src={src}
                 alt="bird"
                 width="200"
                 height="200" />
-            <div className="player__guess">
-                <h3 className="player__title">
+            <div className="guess__wrap">
+                <h3 className="guess__title">
                     {birdName}
                 </h3>
+                <AudioPlayer
+                    src={audioSource}
+                    showJumpControls={false}
+                    showSkipControls={false}
+                    showLoopControl={false}
+                    // other props here
+                />
+
+                <ReactAudioPlayer
+                src={audioSource}
+                controls
+                />
+                {/* <div id="player"> 
+                    <audio id="audio" src={audioSource} />
+                    
+                    <button id="playpause-guess">
+                        <div className="play-btn"></div>
+                    </button>
+                    
+                    <div id="progressbar-guess"></div>
+                    
+                    <div id="volume-guess"></div>
+                </div>
                 <figure>
                     <figcaption className="hidden"></figcaption>
                     <audio
                         controls
                         src={audioSource}
-                        className="player__audio">
+                        className="guess__audio">
                             Your browser does not support the audio lement.
                     </audio>
-                </figure>
+                </figure> */}
             </div>
         </div>
     );
