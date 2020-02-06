9364a6bcf5c700fcd3e718a32d42cad576cdad74
Aykuli <aykuli@ya.ru>
2020-02-06 22:35:25 +0700

    feat: styling player component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,7 +8,10 @@ import './app.scss';
 const App = () => (
     <div className="container">
         <Header score={15}/>
-        <Player src={15} birdName={'***'} />
+        <Player 
+            src={'./placeholder.jpg'} 
+            birdName={'Соловей'}
+            audioSource={'https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3'} />
     </div>
 );
 
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -3,18 +3,28 @@ import React from 'react';
 import './player.scss';
 
 const Player = (props) => {
-    const { src, birdName } = props;
+    const { src, birdName, audioSource } = props;
     return (
         <div className="player">
             <img
                 className="player__img"
                 src={src}
-                alt="bird" />
+                alt="bird"
+                width="200"
+                height="200" />
             <div className="player__guess">
                 <h3 className="player__title">
                     {birdName}
                 </h3>
-                
+                <figure>
+                    <figcaption className="hidden"></figcaption>
+                    <audio
+                        controls
+                        src={audioSource}
+                        className="player__audio">
+                            Your browser does not support the audio lement.
+                    </audio>
+                </figure>
             </div>
         </div>
     );
