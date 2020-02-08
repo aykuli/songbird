85b49f87b3711a5f4c3376d9da025ebc464725fe
Aykuli <aykuli@ya.ru>
2020-02-08 23:21:15 +0700

    feat: added  logic of showing wrong answers

--- src/components/birds-list/birds-list.jsx
+++ src/components/birds-list/birds-list.jsx
@@ -4,30 +4,29 @@ import './birds-list.scss';
 
 import BirdItem from '../bird-item';
 
-const BirdsList = ({ items, onGetAnswer, indexRight, indexWrong }) => (
-    <ul 
-        className="row__child birds-list"
-        onClick={onGetAnswer}>
-        {
-            items.map((item, i) => {
-                // console.log('indexWrong: ',indexWrong);
-                // console.log('indexRight: ', indexRight);
-                // console.log('i: ', i);
-                // let isWrong = false;
-                // let isRight = false;
-                const isWrong = (i === +indexWrong && indexWrong !== null);
-                let isRight = (i === +indexRight && indexRight !== null);
-                // console.log('isWrong: ', isWrong, '\n');
-                
-                return <BirdItem 
-                    key={i}
-                    i={i}
-                    name={item.name} 
-                    isRight={isRight} 
-                    isWrong={isWrong} />;
-            })
-        }
-    </ul>
-);
+const BirdsList = ({ items, onGetAnswer, indexRight, wrongIndexes }) => {
+    const wrongIndexesArr = Array.from(wrongIndexes);
+    
+    return (
+        <ul 
+            className="row__child birds-list"
+            onClick={onGetAnswer}>
+            {   
+                items.map((item, i) => {
+                    // если текущий индекс есть в массиве неправильных ответов 
+                    let isWrong = (wrongIndexesArr.some(index => i === +index));
+                    let isRight = (i === +indexRight && indexRight !== null);
+                    
+                    return <BirdItem 
+                        key={i}
+                        i={i}
+                        name={item.name} 
+                        isRight={isRight} 
+                        isWrong={isWrong} />;
+                })
+            }
+        </ul>
+    );
+}
 
 export default BirdsList;
