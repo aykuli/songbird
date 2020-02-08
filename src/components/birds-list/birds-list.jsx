0120150a4f2b1b902ef2c9b49ff8c8a20407c493
Aykuli <aykuli@ya.ru>
2020-02-08 19:58:58 +0700

    feat: add function onGetAnswer when clicking on birds list

--- src/components/birds-list/birds-list.jsx
+++ src/components/birds-list/birds-list.jsx
@@ -4,13 +4,14 @@ import './birds-list.scss';
 
 import BirdItem from '../bird-item';
 
-const BirdsList = ({ items }) => (
-    <ul className="row__child birds-list">
+const BirdsList = ({ items, onGetAnswer, isRight }) => (
+    <ul 
+        className="row__child birds-list"
+        onClick={onGetAnswer}>
         {
             items.map((item, i) => {
-                let isRight = false;
                 let isWrong = false;
-                if (i === 2) isRight = true;
+                
                 if (i === 4) isWrong = true;
                 return <BirdItem key={i} name={item.name} isRight={isRight} isWrong={isWrong} />;
             })
